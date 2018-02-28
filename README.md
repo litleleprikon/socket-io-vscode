0e5bcad6f6429a0560a599ed009b1183815a15eb
Emil Sharifullin <litleleprikon@gmail.com>
2018-03-01 03:07:35 +0500

    [TI] Added a bunch of badges [TI] Coverage tool changed to codeclimate

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Socket.io client for VSCode
 Extension to communicate with Socket.io 2 server from VSCode.
 
-[![Build Status](https://travis-ci.org/litleleprikon/socket-io-vscode.svg?branch=master)](https://travis-ci.org/litleleprikon/socket-io-vscode)
-[![codecov](https://codecov.io/gh/litleleprikon/socket-io-vscode/branch/master/graph/badge.svg)](https://codecov.io/gh/litleleprikon/socket-io-vscode)
+[![VSCode Marketplace](http://vsmarketplacebadge.apphb.com/version-short/litleleprikon.socket-io-vscode.svg)](https://marketplace.visualstudio.com/items?itemName=litleleprikon.socket-io-vscode)
+[![VSCode Marketplace Installs](http://vsmarketplacebadge.apphb.com/installs-short/litleleprikon.socket-io-vscode.svg)](https://marketplace.visualstudio.com/items?itemName=litleleprikon.socket-io-vscode)
 
+[![Build Status](https://travis-ci.org/litleleprikon/socket-io-vscode.svg?branch=master)](https://travis-ci.org/litleleprikon/socket-io-vscode)
+[![Maintainability](https://api.codeclimate.com/v1/badges/c3f47470bc7b96014a29/maintainability)](https://codeclimate.com/github/litleleprikon/socket-io-vscode/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/c3f47470bc7b96014a29/test_coverage)](https://codeclimate.com/github/litleleprikon/socket-io-vscode/test_coverage)
+[![Known Vulnerabilities](https://snyk.io/test/github/litleleprikon/socket-io-vscode/badge.svg?targetFile=package.json)](https://snyk.io/test/github/litleleprikon/socket-io-vscode?targetFile=package.json)
+[![Percentage of issues still open](http://isitmaintained.com/badge/open/litleleprikon/socket-io-vscode.svg)](http://isitmaintained.com/project/litleleprikon/socket-io-vscode "Percentage of issues still open")
 ---
 
 This extension allows to establish connection to Socket.io server, emit events and receive events from server.
---
+++ src/SocketIOEventDataContentProvider.ts
@@ -0,0 +1,99 @@
+// 'use strict';
+
+// import * as vscode from 'vscode';
+// import * as fs from 'fs';
+// import { sprintf } from 'sprintf-js';
+
+// import { getFileSize, getBuffer } from './util';
+
+// var hexdump = require('hexy');
+
+// export default class SocketIOEventDataContentProvider implements vscode.TextDocumentContentProvider {
+    
+//     private static s_instance: SocketIOEventDataContentProvider = null;
+//     private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
+
+//     constructor() {
+//         if(SocketIOEventDataContentProvider.s_instance) {
+//             SocketIOEventDataContentProvider.s_instance.dispose();
+//         }
+//         SocketIOEventDataContentProvider.s_instance = this;
+//     }
+
+//     static get instance() {
+//         return SocketIOEventDataContentProvider.s_instance;
+//     }
+
+//     public dispose() {
+//         this._onDidChange.dispose();
+//         if(SocketIOEventDataContentProvider.s_instance) {
+//             SocketIOEventDataContentProvider.s_instance.dispose();
+//             SocketIOEventDataContentProvider.s_instance = null;
+//         }
+//     }
+
+//     public provideTextDocumentContent(uri: vscode.Uri): Thenable<string> {
+//         const config = vscode.workspace.getConfiguration('hexdump');
+//         const hexLineLength = config['width'] * 2;
+//         const firstByteOffset = config['showAddress'] ? 10 : 0;
+//         const lastByteOffset = firstByteOffset + hexLineLength + hexLineLength / config['nibbles'] - 1;
+//         const firstAsciiOffset = lastByteOffset + (config['nibbles'] == 2 ? 4 : 2);
+//         const lastAsciiOffset = firstAsciiOffset + config['width'];
+//         const charPerLine = lastAsciiOffset + 1;
+//         const sizeWarning = config['sizeWarning'];
+//         const sizeDisplay = config['sizeDisplay'];
+
+//         return new Promise( async (resolve) => {
+//             let hexyFmt = {
+//                 format      : config['nibbles'] == 8 ? 'eights' : 
+//                             config['nibbles'] == 4 ? 'fours' : 
+//                             'twos',
+//                 width       : config['width'],
+//                 caps        : config['uppercase'] ? 'upper' : 'lower',
+//                 numbering   : config['showAddress'] ? "hex_digits" : "none",
+//                 annotate    : config['showAscii'] ? "ascii" : "none",
+//                 length      : sizeDisplay
+//             };
+
+//             let header = config['showOffset'] ? this.getHeader() : "";
+//             let tail = '(Reached the maximum size to display. You can change "hexdump.sizeDisplay" in your settings.)';
+
+//             let proceed = getFileSize(uri) < sizeWarning ? 'Open' : await vscode.window.showWarningMessage('File might be too big, are you sure you want to continue?', 'Open');
+//             if (proceed == 'Open') {
+//                 let buf = getBuffer(uri);
+//                 let hexString = header;
+//                 hexString += hexdump.hexy(buf, hexyFmt).toString();
+//                 if (buf.length > sizeDisplay) {
+//                     hexString += tail;
+//                 }
+
+//                 return resolve(hexString);
+//             } else {
+//                 return resolve('(hexdump cancelled.)');
+//             }
+//         });
+//     }
+    
+//     get onDidChange(): vscode.Event<vscode.Uri> {
+//         return this._onDidChange.event;
+//     }
+    
+//     public update(uri: vscode.Uri) {
+//         this._onDidChange.fire(uri);
+//     }
+
+//     private getHeader(): string {
+//         const config = vscode.workspace.getConfiguration('hexdump');
+//         let header = config['showAddress'] ? "  Offset: " : "";
+
+//         for (var i = 0; i < config['width']; ++i) {
+//             header += sprintf('%02X', i);
+//             if ((i+1) % (config['nibbles'] / 2) == 0) {
+//                 header += ' ';
+//             }
+//         }
+
+//         header += "\t\n";
+//         return header;
+//     }
+// }
